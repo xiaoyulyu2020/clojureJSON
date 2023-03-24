c852becb296fa0106ad8e3c18eec4cd6bf235090
XiaoyuLyu <lyu.xiaoyu2@mail.dcu.ie>
2023-03-24 21:28:54 +0000

    11

--- json_app/src/json_app/core.clj
+++ json_app/src/json_app/core.clj
@@ -6,50 +6,92 @@
 
 
 
-(def all-records (json/read-str (slurp "/home/xlyu/Downloads/operator-mappings.json")
+(def operator-mappings (json/read-str (slurp "/home/xlyu/Downloads/operator-mappings.json")
                 :key-fn keyword))
 
 ;1 list all countries
 
-(defn getCountries 
-  [data]
-  (into []  (map #(% :country) data)) ) 
+(defn get-all-countries 
+  [operator-mappings]
+  (into
+   []
+   (map :country operator-mappings)) )
+
 
 ;2  List all unique Countries sorted alphabetically
 
-(defn getUniqCountries 
-  [data]
-  (sort (distinct (getCountries data))))
+(defn get-uniq-countries
+  [operator-mappings]
+  (sort
+   (distinct
+    (get-all-countries operator-mappings))))
 
 
-;3. List all networks within a country {“Ireland” [“O2", “Vodafone”], “Spain” [“Orange”]}
-(defn addNetwork [ls data]
-  (let [bindingFirstJSON (first data)
-        cournty (get :country bindingFirstJSON)
-        network (get :name bindingFirstJSON)]
-    reduce---
-    )
-  )
+; ;3. List all networks within a country {“Ireland” [“O2", “Vodafone”], “Spain” [“Orange”]}
 
-(defn getNetworks
-  [data]
-  (def ls (zipmap (getUniqCountries data) (repeat [])))
-  ;; one go here
-  ;create {:Afghanistan [] ....}
-  (addNetwork ls data)
+(defn country-operators 
+  [operator-mappings]
+  (reduce 
+   (fn 
+     [country-operators operator-mapping]
+     (let [country (:country operator-mapping) ;get "Agentina"
+          operators (get country-operators country);get {} "Agentina"=>nil
+          operators (if operators operators []);if operator is nil, return [], otherwise return operator
+          ]
+     (assoc country-operators (:country operator-mapping) (conj operators (:name operator-mapping))))) 
+   {}
+   operator-mappings)
 )
 
-filter
-reduce
-->
-
+; {
+;     "tadig": [
+;       "ARGTM",
+;       "ARG01"
+;     ],
+;     "name": "Telef\u00f3nica M\u00f3viles Argentina S.A.",
+;     "iso3": "ARG",
+;     "e164": [
+;       "5407"
+;     ],
+;     "e212": [
+;       "72207",
+;       "722010"
+;     ],
+;     "realm": [
+;       "epc.mnc007.mcc722.3gppnetwork.org"
+;     ],
+;     "country": "Argentina",
+;     "iso2": "AR"
+;   },
+;   {
+;     "tadig": [
+;       "ROM05"
+;     ],
+;     "name": "S.C. RCS & RDS S.A.",
+;     "iso3": "ROU",
+;     "e164": [
+;       "40770"
+;     ],
+;     "e212": [
+;       "22605"
+;     ],
+;     "realm": [
+;       "epc.mnc005.mcc226.3gppnetwork.org"
+;     ],
+;     "country": "Romania",
+;     "iso2": "RO"
+;   },
 
 ; 4. For every Country that contains a Vodafone network, produce a map of country to a map value that contains a count of all networks for that country and the mapping info for its vodafone network, but remove tadig and lowercase the country name. e.g. {“ireland” {:count 10 :vodafone-mapping {:iso2 “IE”, :country “ireland” …..}}
+
+
 ; 5. Create a map of tadigs to their network names, for every single tadig
 ; 6. Create a map of country name to iso2
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (getUniqCountries all-records)
+  [& args] 
+  (country-operators operator-mappings)
   )
+
+(-main)
